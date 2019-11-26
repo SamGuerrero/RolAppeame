05af3d3064295407a4fa0ae3cdfa9351f68e8e3b
Samantha <gsamguerrero@outlook.es>
2019-11-26 19:58:55 +0100

    Hecho lo que hace cada botón menos el Continuar

--- app/src/main/java/es/centroafuera/rolappeame/CrearPersonaje.java
+++ app/src/main/java/es/centroafuera/rolappeame/CrearPersonaje.java
@@ -1,10 +1,13 @@
 package es.centroafuera.rolappeame;
 
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -129,21 +132,80 @@ public class CrearPersonaje extends AppCompatActivity implements View.OnClickLis
 
             //TODO: Cambiar el textView en función de si aumenta o disminuye su atributo
             case R.id.BTmasAgilidad:
+                TextView puntosAgilidad = findViewById(R.id.puntosAgilidad);
+                sumarPuntos(puntosAgilidad);
 
+                break;
             case R.id.BTmenosAgilidad:
+                TextView puntosagilidad = findViewById(R.id.puntosAgilidad);
+                restarPuntos(puntosagilidad);
+
+                break;
             case R.id.BTmasCarisma:
+                TextView puntosCarisma = findViewById(R.id.puntosCarisma);
+                sumarPuntos(puntosCarisma);
+
+                break;
             case R.id.BTmenosCarisma:
+                TextView puntoscarisma = findViewById(R.id.puntosCarisma);
+                restarPuntos(puntoscarisma);
+
+                break;
             case R.id.BTmasConstitucion:
+                TextView puntosConstitucion = findViewById(R.id.puntosConstitucion);
+                sumarPuntos(puntosConstitucion);
+
+                break;
             case R.id.BTmenosConstitucion:
+                TextView puntosconstitucion = findViewById(R.id.puntosConstitucion);
+                restarPuntos(puntosconstitucion);
+
+                break;
             case R.id.BTmasFuerza:
+                TextView puntosFuerza = findViewById(R.id.puntosFuerza);
+                sumarPuntos(puntosFuerza);
+
+                break;
             case R.id.BTmenosFuerza:
+                TextView puntosfuerza = findViewById(R.id.puntosFuerza);
+                restarPuntos(puntosfuerza);
+
+                break;
             case R.id.BTmasInteligencia:
+                TextView puntosInteligencia = findViewById(R.id.puntosInteligencia);
+                sumarPuntos(puntosInteligencia);
+
+                break;
             case R.id.BTmenosInteligencia:
+                TextView puntosinteligencia = findViewById(R.id.puntosInteligencia);
+                restarPuntos(puntosinteligencia);
+
+                break;
             case R.id.BTmasPercepcion:
-            case R.id.BTmenosPercepcion: break;
+                TextView puntosPercepcion = findViewById(R.id.puntosPercepcion);
+                sumarPuntos(puntosPercepcion);
+
+                break;
+            case R.id.BTmenosPercepcion:
+                TextView puntospercepcion = findViewById(R.id.puntosPercepcion);
+                restarPuntos(puntospercepcion);
+
+                break;
 
             default: break;
         }
 
+    }
+
+    public void sumarPuntos(TextView pantalla){
+        int puntos = Integer.parseInt(String.valueOf(pantalla));
+        puntos++;
+        pantalla.setText(puntos);
+    }
+
+    public void restarPuntos(TextView pantalla){
+        int puntos = Integer.parseInt(String.valueOf(pantalla));
+        puntos--;
+        pantalla.setText(puntos);
     }
 }
